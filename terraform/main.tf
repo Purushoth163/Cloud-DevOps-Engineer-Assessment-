c707c06c354e4028a8e04da48f89ac5383d03315
Your Name <you@example.com>
2024-12-21 22:09:50 +0530

    fifth commit for Kubernetes web app deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,27 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "~> 3.0"
 
-  name = "k8s-vpc"
-  cidr = "10.0.0.0/16"
+#   name = "k8s-vpc"
+#   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+#   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  tags = {
-    Environment = "dev"
-  }
+#   enable_nat_gateway = true
+#   tags = {
+#     Environment = "dev"
+#   }
+# }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
 }
 
 module "eks" {
@@ -20,10 +29,10 @@ module "eks" {
   cluster_name    = "k8s-cluster"
   cluster_version = "1.26"
 
-  subnet_ids         = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+  subnet_ids = [aws_subnet.main.id]
+  vpc_id     = aws_vpc.main.id
 
-    eks_managed_node_groups = {
+  eks_managed_node_groups = {
     eks_nodes = {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["m5.xlarge"]
@@ -38,7 +47,7 @@ module "eks" {
 resource "aws_security_group" "alb_sg" {
   name        = "alb-security-group"
   description = "Allow HTTP and HTTPS traffic"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 80
@@ -67,7 +76,7 @@ resource "aws_lb" "app_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = module.vpc.public_subnets
+  subnets            = [aws_subnet.main.id]
 
   enable_deletion_protection = false
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,7 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = aws_vpc.main.id
 }
 
 output "cluster_name" {
   value = module.eks.cluster_name
-}
-
-output "kubeconfig" {
-  value = module.eks.kubeconfig
 }
